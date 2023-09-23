182ab11af919932fd0bb08c6ca8cac94e193021e
VhungSIK <hungyn12.34n@gmail.com>
2023-09-23 18:58:52 +0700

    khai bao Api và goi click call o main

--- app/src/main/java/com/example/callapircv/MainActivity.java
+++ app/src/main/java/com/example/callapircv/MainActivity.java
@@ -6,9 +6,22 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.example.callapircv.adapter.UserAdapter;
+import com.example.callapircv.api.ApiService;
+import com.example.callapircv.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView rcvUsers;
+    private List<User> mListUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +33,25 @@ public class MainActivity extends AppCompatActivity {
 
         DividerItemDecoration itemDecoration = new DividerItemDecoration(this, DividerItemDecoration.VERTICAL);
         rcvUsers.addItemDecoration(itemDecoration);
+
+        mListUser = new ArrayList<>();
+        callApiUsers();
+    }
+
+    private void  callApiUsers() {
+        ApiService.apiService.getListUssers(1).enqueue(new Callback<List<User>>() {
+            @Override
+            public void onResponse(Call<List<User>> call, Response<List<User>> response) {
+                mListUser = response.body();
+                UserAdapter userAdapter = new UserAdapter(mListUser);
+                rcvUsers.setAdapter(userAdapter);
+            }
+
+            @Override
+            public void onFailure(Call<List<User>> call, Throwable t) {
+                Toast.makeText(MainActivity.this, "Eror", Toast.LENGTH_LONG).show();
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/callapircv/api/ApiService.java
@@ -0,0 +1,28 @@
+package com.example.callapircv.api;
+
+import com.example.callapircv.model.User;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Query;
+
+public interface ApiService {
+
+    Gson gson = new GsonBuilder().setDateFormat("đ-MM-yy").create();
+
+    ApiService apiService = new Retrofit.Builder()
+            .baseUrl("https://jsonplaceholder.typicode.com/")
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+            .create(ApiService.class);
+
+    @GET("posts")
+    Call<List<User>> getListUssers(@Query("userId") int userId);
+}
